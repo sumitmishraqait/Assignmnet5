cf0abdb24cb07004d2856836d2b2c97da4337876
Sumit Mishra <sumitmishra@QAIT.COM>
2017-07-27 18:03:18 +0530

    updated assignment5

--- assignment5/src/com/qainfotech/Assignment_tatoc/Advance.java
+++ assignment5/src/com/qainfotech/Assignment_tatoc/Advance.java
@@ -1,25 +1,23 @@
 package com.qainfotech.Assignment_tatoc;
-
-import org.testng.annotations.Test;
-
-import static org.testng.Assert.assertEquals;
-
-import java.util.ArrayList;
+import java.io.File;
 import java.io.IOException;
+import static com.jayway.restassured.RestAssured.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
+import java.util.Scanner;
 import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.Cookie;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Action;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.jayway.restassured.response.Response;
 
 public class Advance {
 	WebDriver driver;
@@ -74,4 +72,37 @@ public class Advance {
 	  operation.perform(prop,"click","btnsubmit","");
 	  //return driver.getTitle();
 	    }
+	
+	@Test(priority=3)
+	 void restful() throws IOException, InterruptedException {
+  	  prop = objects.getproperties();
+  	  operation.perform(prop, "gotourl", "", "http://10.0.1.86/tatoc/advanced/rest/#");
+  	  driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+  	  String sessionid = operation.gettextofelement(prop, "sessionid").substring(12);
+  	  Response res = given().when().get("http://10.0.1.86/tatoc/advanced/rest/service/token/{sessionid}", sessionid);
+  	  String token = res.path("token");
+  	  given().contentType("application/json").when()
+  	    .post("http://10.0.1.86/tatoc/advanced/rest/service/register?signature=" + token + "&id=" + sessionid
+  	      + "&allow_access=1")
+  	    .then().assertThat().statusCode(200);
+  	  operation.perform(prop, "click", "proceed", "");
+  	  Thread.sleep(2000);
+  	 }
+	
+	@Test(priority=4)
+	void filehandle() throws IOException, InterruptedException {
+		  prop = objects.getproperties();
+		  operation.perform(prop, "click", "download", "");
+		  Thread.sleep(3000);
+		  File file= new File("C:\\Users\\sumitmishra.QAIT\\workspace\\assignment5\\src\\file_handle_test.dat");
+		  Scanner sc=new Scanner(file);
+		  String data= null;
+		  while(sc.hasNext()) {
+		    data=sc.next();
+		  }
+		  sc.close();
+		  operation.perform(prop, "settext", "sign", data);
+		  operation.perform(prop, "click", "proc", "");
+		  file.delete();
+		 }
 }
